c6fb9be3214123877ce75a4b3d5dc0aa9b49fcfc
Iain Ballard <iain.ballard@bjss.com>
2021-01-11 15:41:10 +0000

    basic setup

---
+++ Raid6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RaidRecoverDemo
+{
+    public class Raid6
+    {
+        public byte[]? Data1 { get; set; }
+        public byte[]? Data2 { get; set; }
+        public byte[]? Data3 { get; set; }
+        public byte[]? ParityData { get; set; }
+        public byte[]? ReedSolomon { get; set; }
+        
+        public static Raid6 CalculateRaidSet(byte[] originalData)
+        {
+            var (slice1, slice2, slice3) = Raid6Calculator.SliceData(originalData);
+            var pd = Raid6Calculator.CalculatePd(slice1, slice2, slice3);
+            var rs = Raid6Calculator.CalculateRs(slice1, slice2, slice3);
+            return new Raid6(slice1, slice2, slice3, pd, rs);
+        }
+        
+        public Raid6(byte[] d1, byte[] d2, byte[] d3, byte[] pd, byte[] rs)
+        {
+            Data1 = d1;
+            Data2 = d2;
+            Data3 = d3;
+            ParityData = pd;
+            ReedSolomon = rs;
+        }
+
+        public byte[] Recover()
+        {
+            if (Data1 != null && Data2 != null && Data3 != null) return Raid6Calculator.UnsliceData(Data1!, Data2!, Data3!);
+            throw new NotImplementedException("Recovery not yet implemented");
+        }
+    }
+}
---
+++ SetupAndRecoveryTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RaidRecoverDemo
+{
+    [TestFixture]
+    public class SetupAndRecoveryTest
+    {
+
+        [Test]
+        public void can_setup_and_recover_a_dataset()
+        {
+            // Based on https://anadoxin.org/blog/error-recovery-in-raid6.html/
+            var rnd = new Random();
+            
+            var originalData = new byte[3072]; // easy to slice 3 ways. TODO: handle non-factor-3 sizes
+            rnd.NextBytes(originalData);
+            
+            // Calculate the recovery data
+            var raidSet = Raid6.CalculateRaidSet(originalData);
+            Assert.That(raidSet, Is.Not.Null, "Failed to generate raid set");
+            
+            // Main 'reading' call
+            var recoveredData = raidSet.Recover();
+            Assert.That(recoveredData, Is.EqualTo(originalData).AsCollection!, "Recovered data is not the same as before");
+            
+            // TODO: null out various chunks and get them recovered
+            raidSet.Data1 = null;
+            recoveredData = raidSet.Recover();
+            Assert.That(recoveredData, Is.EqualTo(originalData).AsCollection!, "Recovered data is not the same as before");
+            
+            // Check data is back:
+            var result = Raid6Calculator.UnsliceData(raidSet.Data1!, raidSet.Data2!, raidSet.Data3!);
+            Assert.That(result, Is.EqualTo(originalData).AsCollection!, "Recover did not reset data");
+            
+        }
+    }
+}
