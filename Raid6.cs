ece38221c554da8e81bfb60916f35b3f5c00b24f
Iain Ballard <iain.ballard@bjss.com>
2021-01-12 09:39:06 +0000

    Some recovery

--- Raid6.cs
+++ Raid6.cs
@@ -4,6 +4,7 @@ namespace RaidRecoverDemo
 {
     public class Raid6
     {
+        private readonly int _sliceSize;
         public byte[]? Data1 { get; set; }
         public byte[]? Data2 { get; set; }
         public byte[]? Data3 { get; set; }
@@ -25,12 +26,66 @@ namespace RaidRecoverDemo
             Data3 = d3;
             ParityData = pd;
             ReedSolomon = rs;
+            _sliceSize = d1.Length;
         }
 
         public byte[] Recover()
         {
-            if (Data1 != null && Data2 != null && Data3 != null) return Raid6Calculator.UnsliceData(Data1!, Data2!, Data3!);
-            throw new NotImplementedException("Recovery not yet implemented");
+            if (Data1 != null && Data2 != null && Data3 != null)
+            {
+                // TODO: check the data is CORRECT against the PD, use RS to recover
+                
+                ParityData ??= Raid6Calculator.CalculatePd(Data1, Data2, Data3);
+                ReedSolomon ??= Raid6Calculator.CalculateRs(Data1, Data2, Data3);
+                return Raid6Calculator.UnsliceData(Data1!, Data2!, Data3!);
+            }
+            
+            var lostParity = ParityData == null;
+            var lostCodes = ReedSolomon == null;
+            var lostRecovery = lostParity && lostCodes;
+            var lostData = 0;
+            if (Data1 == null) lostData++;
+            if (Data2 == null) lostData++;
+            if (Data3 == null) lostData++;
+            
+            // Reject impossible cases
+            if (lostData > 2) throw new Exception("Too much data lost. Recovery not possible");
+            if (lostData > 0 && lostRecovery) throw new Exception("Data and recovery codes lost. Recovery not possible");
+            if (lostData > 1 && (lostParity || lostCodes)) throw new Exception("Too much data and recovery info lost. Recovery not possible");
+            
+            // Possible cases left:
+            // 1) 2 data, PD
+            // 2) 2 data, RS
+            // 3) 1 data, PD & RS
+
+            if (lostData == 1 && !lostParity)
+            {
+                RecoverDataFromParity();
+                
+                ParityData ??= Raid6Calculator.CalculatePd(Data1!, Data2!, Data3!);
+                ReedSolomon ??= Raid6Calculator.CalculateRs(Data1!, Data2!, Data3!);
+                return Raid6Calculator.UnsliceData(Data1!, Data2!, Data3!);
+            }
+
+            throw new NotImplementedException("This recovery not yet implemented");
+        }
+
+        private void RecoverDataFromParity()
+        {
+            var recovered = new byte[_sliceSize];
+            var parity = ParityData;
+            var good1 = Data1 ?? Data2;
+            var good2 = Data3 ?? Data2;
+
+            if (parity == null || good1 == null || good2 == null) throw new Exception("Recover data from parity called with invalid state");
+            for (var i = 0; i < _sliceSize; i++)
+            {
+                recovered[i] = (byte)(good1[i] ^ good2[i] ^ parity[i]);
+            }
+            
+            Data1 ??= recovered;
+            Data2 ??= recovered;
+            Data3 ??= recovered;
         }
     }
 }
--- SetupAndRecoveryTest.cs
+++ SetupAndRecoveryTest.cs
@@ -25,14 +25,70 @@ namespace RaidRecoverDemo
             Assert.That(recoveredData, Is.EqualTo(originalData).AsCollection!, "Recovered data is not the same as before");
             
             // TODO: null out various chunks and get them recovered
+            
+            // 1) Parity recovery
+            raidSet.ParityData = null;
+            recoveredData = raidSet.Recover();
+            Assert.That(recoveredData, Is.EqualTo(originalData).AsCollection!, "Recovered data is not the same as before");
+            Assert.That(raidSet.ParityData, Is.Not.Null, "Parity was not recovered");
+            
+            // 2) Reed-Solomon recovery
+            raidSet.ReedSolomon = null;
+            recoveredData = raidSet.Recover();
+            Assert.That(recoveredData, Is.EqualTo(originalData).AsCollection!, "Recovered data is not the same as before");
+            Assert.That(raidSet.ReedSolomon, Is.Not.Null, "Parity was not recovered");
+            
+            // 3a) Single data recovery
             raidSet.Data1 = null;
             recoveredData = raidSet.Recover();
             Assert.That(recoveredData, Is.EqualTo(originalData).AsCollection!, "Recovered data is not the same as before");
-            
             // Check data is back:
             var result = Raid6Calculator.UnsliceData(raidSet.Data1!, raidSet.Data2!, raidSet.Data3!);
             Assert.That(result, Is.EqualTo(originalData).AsCollection!, "Recover did not reset data");
             
+            // 3b) Single data recovery
+            raidSet.Data2 = null;
+            recoveredData = raidSet.Recover();
+            Assert.That(recoveredData, Is.EqualTo(originalData).AsCollection!, "Recovered data is not the same as before");
+            // Check data is back:
+            result = Raid6Calculator.UnsliceData(raidSet.Data1!, raidSet.Data2!, raidSet.Data3!);
+            Assert.That(result, Is.EqualTo(originalData).AsCollection!, "Recover did not reset data");
+            
+            // 3c) Single data recovery
+            raidSet.Data3 = null;
+            recoveredData = raidSet.Recover();
+            Assert.That(recoveredData, Is.EqualTo(originalData).AsCollection!, "Recovered data is not the same as before");
+            // Check data is back:
+            result = Raid6Calculator.UnsliceData(raidSet.Data1!, raidSet.Data2!, raidSet.Data3!);
+            Assert.That(result, Is.EqualTo(originalData).AsCollection!, "Recover did not reset data");
+            
+            
+            // 4a) Double data recovery
+            raidSet.Data1 = null;
+            raidSet.Data2 = null;
+            recoveredData = raidSet.Recover();
+            Assert.That(recoveredData, Is.EqualTo(originalData).AsCollection!, "Recovered data is not the same as before");
+            // Check data is back:
+            result = Raid6Calculator.UnsliceData(raidSet.Data1!, raidSet.Data2!, raidSet.Data3!);
+            Assert.That(result, Is.EqualTo(originalData).AsCollection!, "Recover did not reset data");
+            
+            // 4b) Double data recovery
+            raidSet.Data1 = null;
+            raidSet.Data3 = null;
+            recoveredData = raidSet.Recover();
+            Assert.That(recoveredData, Is.EqualTo(originalData).AsCollection!, "Recovered data is not the same as before");
+            // Check data is back:
+            result = Raid6Calculator.UnsliceData(raidSet.Data1!, raidSet.Data2!, raidSet.Data3!);
+            Assert.That(result, Is.EqualTo(originalData).AsCollection!, "Recover did not reset data");
+            
+            // 4c) Double data recovery
+            raidSet.Data2 = null;
+            raidSet.Data3 = null;
+            recoveredData = raidSet.Recover();
+            Assert.That(recoveredData, Is.EqualTo(originalData).AsCollection!, "Recovered data is not the same as before");
+            // Check data is back:
+            result = Raid6Calculator.UnsliceData(raidSet.Data1!, raidSet.Data2!, raidSet.Data3!);
+            Assert.That(result, Is.EqualTo(originalData).AsCollection!, "Recover did not reset data");
         }
     }
 }
